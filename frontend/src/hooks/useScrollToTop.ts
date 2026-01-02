c81a39634d3a08f97a2e05ce401d9be281b24201
rendybp <rendibuanap@gmail.com>
2026-01-02 16:17:17 +0700

    fix: use hash so page can scroll up to top if hash is off (hash example: /#features)

--- frontend/src/hooks/useScrollToTop.ts
+++ frontend/src/hooks/useScrollToTop.ts
@@ -2,9 +2,11 @@ import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
 export const useScrollToTop = () => {
-  const { pathname } = useLocation();
+  const { pathname, hash } = useLocation();
 
   useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [pathname]);
+    if (!hash) {
+      window.scrollTo(0, 0);
+    }
+  }, [pathname, hash]);
 };
