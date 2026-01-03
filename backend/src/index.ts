ef008b18db4b5935c5a7f5066ca8955f5838b1f3
rendybp <rendibuanap@gmail.com>
2026-01-04 02:02:37 +0700

    fix: delete customjs from cdn, hope its fix the swagger problem

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -103,10 +103,6 @@ const swaggerUiOptions = {
   customCss: '.swagger-ui.topbar { display: none }',
   customSiteTitle: "Vehicle Tracker API Documentation",
   customCssUrl: "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css",
-  customJs: [
-    "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.js",
-    "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.js",
-  ],
   swaggerOptions: {
     persistAuthorization: true, // Persist authorization data
     displayRequestDuration: true,
