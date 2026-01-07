e73c9ad6f89ebaab94ef5a0237fda2a672709487
rendybp <rendibuanap@gmail.com>
2026-01-07 19:04:04 +0700

    fix: axios error, solve by adding remove trailing slashes logic

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 import { useAuthStore } from "../stores/authStore";
 
-const baseURL = import.meta.env.VITE_API_URL || "http://localhost:5000";
+const rawBaseURL = import.meta.env.VITE_API_URL || "http://localhost:5000";
+
+const baseURL = rawBaseURL.replace(/\/+$/, ""); // Remove trailing slashes
 
 export const api = axios.create({
     baseURL,
